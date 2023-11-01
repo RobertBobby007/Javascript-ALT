5674c99a57dc5a0951c7937a41589c444e4b5a2f
1123 <1123@student.itgymnazium.cz>
2023-11-01 11:27:55 +0100

    test

---
+++ 01promeny.js
@@ -0,0 +1,48 @@
+let massage;
+
+massage = "Hello"
+
+console.log(massage)
+// proměná v jednou řádku
+
+let massage2 = "Ahoj"
+
+console.log(massage2)
+
+//var vs let
+
+var massage3 = "Zdar" //var nepoužívat
+
+console.log(massage3)
+
+//jmeno proměné
+// jen pisma, číslice, _
+
+let$d = 5
+
+console.log($d)
+
+let adminUser = "Chuck Norris"
+
+let omegaw = "ok"
+
+console.log(omegaw)
+
+//konstanty
+
+const myBirthday = "10.01.2007"
+
+console.log(myBirthday)
+
+myBirthday = "11.01.2007"
+
+console.log(myBirthday)
+
+console.log(MY_BIRTHDAY = "11.01.2007"
+)
+
+console.log(MY_BIRTHDAY)
+
+let actorName = "Bobin"
+
+let shoppingCart = []
---
+++ 01strukturaKodu.js
@@ -0,0 +1,9 @@
+console.log("Hello"); console.log("World"); //v node.js nefunguje alert. Každý středník je nový řádek
+
+console.log("Hello"); 
+console.log("World");
+
+console.log("Hello")
+console.log("World")
+
+console.log(3+1+2)
