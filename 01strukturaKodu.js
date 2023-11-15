2296a964e72c6b1c5c911a00fcf36e333f13a203
Semtex <kopecky.d@gmail.com>
2023-11-15 11:13:41 +0100

    Hodina 1 až 3

---
+++ 01strukturaKodu.js
@@ -0,0 +1,21 @@
+console.log("Hello"); console.log("World");
+
+console.log("Hello");
+console.log("World");
+
+console.log("Hello")
+console.log("World")
+
+
+console.log(3 +
+    1
+    + 2)
+
+//jednořádkový komentář
+
+/*
+Lorem ipsum dolor sit amet, 
+consectetuer adipiscing elit.
+ Mauris metus. 
+ Praesent vitae arcu tempor neque lacinia pretium. 
+ */
---
+++ 01variables.js
@@ -0,0 +1,46 @@
+let message;
+
+message = "Hello"
+
+console.log(message)
+
+//proměnná v jenom řádku
+
+let message2 = "Ahoj"
+
+console.log(message2)
+
+// var vs let
+var message3 = "Zdar" //var nepoužívat!!!
+
+console.log(message3)
+
+//jméno proměnné
+// jen písmna, číslice, $, _
+
+let $d = 5
+
+console.log($d)
+
+let adminUser = "Chuck Norris"
+
+let omegaω = "Ok"
+
+console.log(omegaω)
+
+//konstanty
+
+const MY_BIRTHDAY = "1.7.1991"
+
+console.log(MY_BIRTHDAY = "1.7.1991"
+)
+
+// Y_BIRTHDAY = "1.8.1970" vyhodí chybu
+
+console.log(MY_BIRTHDAY)
+
+let actorName = "Arnold"
+
+let shoppingCart = []
+
+let variableString = `${actorName}`
