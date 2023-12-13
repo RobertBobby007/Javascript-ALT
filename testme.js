d5959cfd6cf2cac8da53ff7863891237cfe67d58
1123 <1123@student.itgymnazium.cz>
2023-12-13 11:48:00 +0100

    update0.1.0

---
+++ testme.js
@@ -0,0 +1,24 @@
+//tento kód slouží pouze jako na forms
+
+
+//let i = 3;
+
+//while (i){
+ //   console.log(i-- );
+//}
+
+//let i = 0;
+//while (++i < 5) console.log(i);
+
+//let i = 0;
+//while (i++ < 5) console.log(i);
+
+//let i = 0;
+//while(++i < 5) console.log(i);
+
+//let i = 0;
+//while (i++ < 5) console.log(i);
+
+//for (let i = 0; i < 5; i++) console.log(i);
+
+//for (let i = 0; i < 5; ++i) console.log(i);
