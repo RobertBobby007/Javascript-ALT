105b14b47336d97a48e79455c5ebb5c7b4c628ad
1123 <1123@student.itgymnazium.cz>
2024-01-10 11:07:58 +0100

    update

---
+++ 07code_quality.js
@@ -0,0 +1,41 @@
+//Kód musí být čitelný a srozumitelný
+//složené závorky
+if (true) {
+    console.log('kód');
+}
+
+
+//jednořádkové podmínky - bez složené závorek (nedělat je to nepřehledné)
+if (true) console.log('kód')
+
+//více řádková podmínka - s odsazením a se závorkou
+if (true) {
+    console.log('kód')
+}
+
+
+//délka řádku - max 80 znaků
+const string = "lhdaoiehf asdfjadsoi fhasfuasdu foashif adsf oads fhadsfi oadsufnadshfoadsfadszfuads foasufu" +
+    "jfsldfadslkfsfjadslkfladskfkladsfkladshflkadshflkadshkfladsfklasfloasflofjasphfphadsfpiafhůadofo§adsfnhdaspg" +
+    "sldfjladsjfasoifhjasdifjladsjfladsfladsjfladsjfldslksajfklasdlkfjadsjfadsjfůadsfjksdljfůadslfághíaepůrioasmdivé" +
+    "sfojoaezioáhíahfndasviadozgasdzhnasiodfldsýífaszehfnoiadsoůfpíadasdoifjadsfuopadsfhasbnfladsufpadszhfadslf.asuís" +
+    "pfisdfasdfadshfhasdoflasihdfladshifsahfhasflihaszfsadhnfoladsifopadsufhoadsfoůadsofiusafasofůasoddfzuespdjfoadsfeu"
+
+
+//dlouhá podmínka - lze rozdělit na více řádku
+if (
+    id === 13 &&
+    moonpage === 'poroz'
+) {
+    letTheSoberynegin();
+}
+
+//klávesolá zkrata - ctrl+alt+l
+
+
+//odsazení mezi funkcemi
+
+// středník se nemusí používat pokud jste zkušení
+
+// linter = program kontroluje kod a upozorní na chyby vzhledu
+//nejpoužívanější je linter = ESLint
