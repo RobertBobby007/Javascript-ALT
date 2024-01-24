51050071912476d7b0119ad28c95fe7545fbbba7
1123 <1123@student.itgymnazium.cz>
2024-01-24 11:05:57 +0100

    update

---
+++ 08comments.js
@@ -0,0 +1,26 @@
+//jedořádkové
+/* víc řídkové kom
+fafas
+fasdff
+afsdfasf
+fadfsf
+sdfasfsf
+sdfdasfsf
+sdfsfasfas
+ */
+
+
+//dobré kom
+//1. vyšší úrovně nebo architektury
+//2. složitější alg
+//3. vysvětlení důležitých rozhnotí
+//4. funkce
+/**
+ * vací
+ * JSdoc.app
+ * @return
+ * @param
+ * @example
+ * parametr {number} x číslo k umocnení
+ */
+
--- 08objekty.js
+++ 08objekty.js
@@ -14,3 +14,48 @@ let person = {
 }
 
 console.log(person);
+
+//přístup k hodnotám objeku
+console.log(person.firstName);
+
+//přidání dvojice klíče: hodnota
+person.job = "CEO";
+
+console.log(person);
+
+person["favorite club"] = "NRW Club VIP ++"
+
+console.log(person);
+
+//přístup k hodnotám objektud
+console.log(person["favorite club"])
+
+//dynamický klíč
+let key = "favorite color";
+person[key] = "red"
+
+console.log(person[key]); //hranatá závorka je bezpečnější
+
+//vymazání dvojice klíč: hodnota
+delete person["age"] // delete person.age
+console.log(person);
+
+//testování přítomnosti vlastnosti, klíče (key, property)
+console.log("favorite club" in person);  //true nebo false vrátí
+console.log("age" in person); // true nebo false
+
+//interace přes objekt
+//iterace přes klíč
+for (let klíč in person){
+    console.log(klíč);
+}
+
+//interace přes hodnoty
+for (let klíč in person){
+    console.log((person[klíč]))
+}
+
+//interace před dvojice klíč: hodnota
+for (let klíč in person){
+    console.log(klíč + ": " + person[klíč])
+}
