04827c550a7e69b0723431d523f4a64ce8f63338
1123 <1123@student.itgymnazium.cz>
2024-01-24 10:44:30 +0100

    update

---
+++ 08objekty.js
@@ -0,0 +1,16 @@
+//vytváření objektů
+let user = new Object(); //takto nepoužívat
+console.log(user);
+let student = {}
+console.log(student);
+
+
+//vytváření neprázdých objektů
+let person = {
+    firstName: "Robert",
+    lastName: "Knobloch",
+    age: 17,
+    sex: "male"
+}
+
+console.log(person);
