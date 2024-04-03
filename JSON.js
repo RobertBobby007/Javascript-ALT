bbdf64b7f5768916f76c98c89a5556993b7a7a2b
1123 <1123@student.itgymnazium.cz>
2024-04-03 10:34:12 +0200

    update

---
+++ JSON.js
@@ -0,0 +1,10 @@
+//JSON - JavaScript Object Notstion
+//stralizace objektů - způsob jak zapsat
+let student = {
+    "name": "Pepa",
+    "lastname": "Chocholatka",
+    "age": 16,
+    "courses": ["swimm", "Czech", "History"]
+
+
+}
