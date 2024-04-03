cfa1c04ae4f31c83bd0adf608279d194a5d9bd6d
1123 <1123@student.itgymnazium.cz>
2024-04-03 10:44:14 +0200

    update

--- JSON.js
+++ JSON.js
@@ -4,7 +4,26 @@ let student = {
     "name": "Pepa",
     "lastname": "Chocholatka",
     "age": 16,
-    "courses": ["swimm", "Czech", "History"]
+    "courses": ["swimm", "Czech", "History"],
 
-
+    parents: [
+        {
+            name: "Pepa",
+            lastname: "Chocholatka"
+        },
+        {
+          name: "Jana",
+          lastname: "Chocolatková"
+        }
+    ]
 }
+
+console.log(JSON.stringify(student))
+
+//ze stringu object
+let str = '{"Brand":"Škoda", "type":"Skala", "colour""Blue"}'
+console.log(str)
+
+let porsche = JSON.parse(str)
+console.log(Škoda)
+console.log(Škoda.type)
